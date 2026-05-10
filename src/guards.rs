9d8c3dfc10c37ab8b6b745873804a5846d1835d7
3BK <40867233+3BK@users.noreply.github.com>
2026-05-10 17:30:45 -0400

    Create guards.rs

---
+++ src/guards.rs
@@ -0,0 +1,20 @@
+use windows_sys::Win32::{
+    Foundation::HANDLE,
+    Security::Cryptography::{CertCloseStore, CertFreeCertificateContext, CERT_CONTEXT, HCERTSTORE},
+};
+use tokio::net::windows::named_pipe::NamedPipeServer;
+
+pub struct PipeGuard<'a>(pub &'a mut NamedPipeServer);
+impl Drop for PipeGuard<'_> {
+    fn drop(&mut self) { let _ = self.0.disconnect(); }
+}
+
+pub struct CertContextGuard(pub *const CERT_CONTEXT);
+impl Drop for CertContextGuard {
+    fn drop(&mut self) { if !self.0.is_null() { unsafe { CertFreeCertificateContext(self.0) }; } }
+}
+
+pub struct CertStoreGuard(pub HCERTSTORE);
+impl Drop for CertStoreGuard {
+    fn drop(&mut self) { if !self.0.is_null() { unsafe { CertCloseStore(self.0, 0) }; } }
+}
